27e83f4441eff70616911aa95c5035cc3f867017
nayan-cv <knowuchiha2881@gmail.com>
2024-07-02 15:14:15 +0530

    added few changes

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -37,7 +37,7 @@ const Business = () => (
 
 		<div
 			className={`${layout.sectionImg} flex-col`}
-			style={{textAlign: 'justify'}}
+			
 		>
 			{features.map((feature, index) => (
 				<FeatureCard key={feature.id} {...feature} index={index}/>
--- src/style.js
+++ src/style.js
@@ -3,8 +3,8 @@ const styles = {
 
   heading2: "font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full",
   heading3: "font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full flex justify-center items-center text-center",
-  paragraph: "font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] text-justify",
-  smallParagraph: "font-poppins font-normal text-dimWhite text-[14px] leading-[30.8px] text-justify",
+  paragraph: "font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]",
+  smallParagraph: "font-poppins font-normal text-dimWhite text-[14px] leading-[30.8px]",
 
   flexCenter: "flex justify-center items-center",
   flexStart: "flex justify-center items-start",
